05b955b1554cb49f06e828b71268e183bb4326b3
Orion Engleton <rsgrafx@gmail.com>
2018-08-09 23:07:01 -0700

    chore: Config setup

--- test/sendle_test.exs
+++ test/sendle_test.exs
@@ -2,7 +2,7 @@ defmodule SendleTest do
   use ExUnit.Case
   doctest Sendle
 
-  test "greets the world" do
-    assert Sendle.hello() == :world
+  test "config keys" do
+    assert %{api_endpoint: _, sendle_auth_id: _, sendle_api_key: _} =  Application.get_env(:sendle, :api_credentials)
   end
 end
