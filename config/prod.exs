a406a80fc09874133b2e0109afc1091d3d2f79c1
Orion Engleton <rsgrafx@gmail.com>
2018-08-13 22:28:39 -0700

    chore: Elixir formatter made changes

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,8 @@
 use Mix.Config
 
-config :sendle, api_credentials: %{
-  api_endpoint: System.get_env("API_SANDBOX"),
-  sendle_auth_id: System.get_env("SENDLE_ID"),
-  sendle_api_key: System.get_env("SENDLE_API_KEY")
-}
+config :sendle,
+  api_credentials: %{
+    api_endpoint: System.get_env("API_SANDBOX"),
+    sendle_auth_id: System.get_env("SENDLE_ID"),
+    sendle_api_key: System.get_env("SENDLE_API_KEY")
+  }
